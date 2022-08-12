28a4615f0460b749b1aecb3cd9b7df03e7ed3cd7
Wickramaranga Abeygunawardhana <w@umstek.tk>
2022-08-12 16:56:52 +0530

    Add parallel connect functionality

--- readme.md
+++ readme.md
@@ -7,7 +7,7 @@ Doesn't log, doesn't print. Just sit in the middle and exchange data.
 
 Configure environment variables or put them in a `.env` file. See `.env.example`.  
 Then start your TCP server at `SEND_ADDR`, and run `forward1`.  
-Then connect your client to `LISTEN_ADDR`. Only 1 client can be connected at a time.
+Then connect your client(s) to `LISTEN_ADDR`.
 
 When your client connects, `forward1` will connect to `SEND_ADDR` and exchange data. It will disconnect when the client does.
 
--- src/main.rs
+++ src/main.rs
@@ -1,26 +1,35 @@
+use chrono::{DateTime, Utc};
 use dotenv::dotenv;
 use std::env;
 use tokio::{
     io::copy_bidirectional,
     net::{TcpListener, TcpStream},
+    spawn,
 };
 
 #[tokio::main]
 async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let now: DateTime<Utc> = Utc::now();
+    // println!("UTC now is: {}", now.format("%Y-%m-%d__%H-%M-%S-%f"));
+    println!("UTC now is: {}", now.to_rfc3339());
+
     dotenv().ok();
     let listen_addr = env::var("LISTEN_ADDR").unwrap_or("127.0.0.1:12369".to_string());
     let send_addr = env::var("SEND_ADDR").unwrap_or("127.0.0.1:12368".to_string());
 
     let listener = TcpListener::bind(listen_addr).await?;
+    println!("Listening on {}", send_addr);
     loop {
-        let (mut downstream, _) = listener.accept().await?;
+        let (mut downstream, client_addr) = listener.accept().await?;
+        println!("Connected to {}", client_addr);
         let mut upstream = TcpStream::connect(send_addr.clone()).await?;
-
-        if copy_bidirectional(&mut upstream, &mut downstream)
-            .await
-            .is_err()
-        {
-            eprintln!("Failed to copy.");
-        }
+        spawn(async move {
+            if copy_bidirectional(&mut upstream, &mut downstream)
+                .await
+                .is_err()
+            {
+                eprintln!("Failed to copy.");
+            }
+        });
     }
 }
